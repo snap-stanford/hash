849a91161f014730c9e4753bf6649abbdcd643da
chantat <c.eksombatchai@gmail.com>
2013-11-05 10:32:19 -0800

    Added hash-test-05.cc

---
+++ hash-test-05.cc
@@ -0,0 +1,77 @@
+#include "../snap/snap-core/Snap.h"
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include <sys/resource.h>
+#include <sys/time.h>
+#include <sys/times.h>
+
+THash<TInt, TInt> h;
+long long n;
+long long num_operation = 100000000;
+
+void Insertion() {
+  // Construct random key sequence
+  TIntV v(n);
+  for (long long i = 0; i < n; i++) {
+    v[i] = i;
+  }
+
+  TRnd rnd;
+  v.Shuffle(rnd);
+
+  struct drand48_data buffer;
+  srand48_r(time(NULL), &buffer);
+
+  // Begin Test
+  long int r;
+  for (long long i = 0; i < n; i++) {
+    lrand48_r(&buffer, &r);
+    h.AddDat(v[i], r);
+  }
+}
+
+
+void RandomAccessTest() {
+  struct drand48_data buffer;
+  srand48_r(time(NULL), &buffer);
+
+  // Begin Test
+  THash<TInt, TInt> stat;
+  long int r;
+  for (long long i = 0; i < num_operation; i++) {
+    lrand48_r(&buffer, &r);
+    int ret = h.GetDat2(r % n);
+    
+    TInt freq;
+    if (!stat.IsKeyGetDat(ret, freq)) {
+      freq = 0;
+    }
+    stat.AddDat(ret, freq + 1);
+  }
+
+  printf("Len\tFreq\n");
+  TIntPrV result;
+  stat.GetKeyDatPrV(result);
+  for (int i = 0; i < result.Len(); i++) {
+    printf("%d\t%d\n", result[i].GetVal1().Val, result[i].GetVal2().Val);
+  } 
+}
+
+int main( int argc, char* argv[] ){
+  if (argc != 2) {
+    exit(0);
+  }
+  sscanf(argv[1], "%lld", &n);
+
+  // Insertion Test
+  Insertion();
+  
+  // Random Access Test
+  RandomAccessTest();
+  return 0;
+}
