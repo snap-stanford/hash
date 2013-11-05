ae1a152cb96ef561990414533dfba013f99dc1ac
chantat <c.eksombatchai@gmail.com>
2013-11-05 12:59:25 -0800

    Added string-hash-test-01 and string-hash-test-02

--- Makefile
+++ Makefile
@@ -4,10 +4,10 @@ LDFLAGS += -lrt
 SNAP = ../snap/snap-core
 GLIB = ../snap/glib-core
 
-all: rand-test vec-test-01 vec-test-02 hash-test-01 hash-test-02 hash-test-03 hash-test-04 hash-test-05 two-vec-test-01
+all: rand-test vec-test-01 vec-test-02 string-hash-test-01 string-hash-test-02 hash-test-01 hash-test-02 hash-test-03 hash-test-04 hash-test-05 two-vec-test-01
 opt: CXXFLAGS += -O3
 opt: LDFLAGS += -O3
-opt: rand-test vec-test-01 vec-test-02 hash-test-01 hash-test-02 hash-test-03 hash-test-04 hash-test-05 two-vec-test-01
+opt: rand-test vec-test-01 vec-test-02 string-hash-test-01 string-hash-test-02 hash-test-01 hash-test-02 hash-test-03 hash-test-04 hash-test-05 two-vec-test-01
 
 rand-test: rand-test.cc Snap.o
 	g++ -o rand-test rand-test.cc Snap.o -I$(GLIB) -I$(SNAP) $(LDFLAGS)
@@ -15,6 +15,10 @@ vec-test-01: vec-test-01.cc Snap.o
 	g++ -o vec-test-01 vec-test-01.cc Snap.o -I$(GLIB) -I$(SNAP) $(LDFLAGS)
 vec-test-02: vec-test-02.cc Snap.o
 	g++ -o vec-test-02 vec-test-02.cc Snap.o -I$(GLIB) -I$(SNAP) $(LDFLAGS)
+string-hash-test-01: string-hash-test-01.cc Snap.o
+	g++ -o string-hash-test-01 string-hash-test-01.cc Snap.o -I$(GLIB) -I$(SNAP) $(LDFLAGS)
+string-hash-test-02: string-hash-test-02.cc Snap.o
+	g++ -o string-hash-test-02 string-hash-test-02.cc Snap.o -I$(GLIB) -I$(SNAP) $(LDFLAGS)
 hash-test-01: hash-test-01.cc Snap.o
 	g++ -o hash-test-01 hash-test-01.cc Snap.o -I$(GLIB) -I$(SNAP) $(LDFLAGS)
 hash-test-02: hash-test-02.cc Snap.o
---
+++ string-hash-test-02.cc
@@ -0,0 +1,203 @@
+#include "../snap/snap-core/Snap.h"
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include <sys/resource.h>
+#include <sys/time.h>
+#include <sys/times.h>
+
+TStrV strings;
+TStrHash<TInt> h;
+long long n;
+long long num_operation = 10000000;
+
+void RandomStrings() {
+  struct drand48_data buffer;
+  srand48_r(time(NULL), &buffer);
+
+  for (long long i = 0; i < n; i++) {
+    static const char alphanum[] =
+      "0123456789"
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+      "abcdefghijklmnopqrstuvwxyz";
+
+    long int len;
+    lrand48_r(&buffer, &len);
+    len = (len % 10) + 5;
+
+    char s[20];
+    for (int i = 0; i < len; ++i) {
+      s[i] = alphanum[rand() % (sizeof(alphanum) - 1)];
+    }
+    s[len] = 0;
+
+    strings.Add(s);
+  }
+}
+
+void getstime(struct timeval *tval, char *s)
+{
+  int msec;
+  char *s1;
+
+  // printf("2. %d %d\n", tval->tv_sec, tval->tv_usec);
+  s1 = strncpy(s, ctime(&tval->tv_sec)+11, 8);
+  s1[8] = '\0';
+  msec = (int) (tval->tv_usec / 1000);
+  sprintf(s, "%s.%03d", s1, msec);
+}
+
+float getcputime() {
+  struct rusage rusage;
+  float result;
+
+  getrusage(RUSAGE_SELF, &rusage);
+
+  result =
+    ((float) (rusage.ru_utime.tv_usec + rusage.ru_stime.tv_usec) / 1000000) +
+    ((float) (rusage.ru_utime.tv_sec + rusage.ru_stime.tv_sec));
+  return result;
+}
+
+float gettimediff(timespec start, timespec end) {
+  timespec temp; 
+  if ((end.tv_nsec-start.tv_nsec)<0) {
+    temp.tv_sec = end.tv_sec-start.tv_sec-1;
+    temp.tv_nsec = 1000000000+end.tv_nsec-start.tv_nsec;
+  } else {
+    temp.tv_sec = end.tv_sec-start.tv_sec;
+    temp.tv_nsec = end.tv_nsec-start.tv_nsec;
+  }
+  return (float)temp.tv_sec + ((float)temp.tv_nsec) / 1000000000;
+}
+
+void InsertionTest() {
+  struct drand48_data buffer;
+  srand48_r(time(NULL), &buffer);
+
+  // Begin Test
+  float fs = getcputime();
+
+  long int r;
+  for (long long i = 0; i < n; i++) {
+    lrand48_r(&buffer, &r);
+    h.AddDat(strings[i], r);
+  }
+
+  // Calculate Stat
+  struct rusage rusage;
+  getrusage(RUSAGE_SELF, &rusage);
+
+  float fe = getcputime();
+  float tdiff = fe - fs;
+
+  struct timeval tval;
+  char tstr[64];
+  gettimeofday(&tval, 0);
+  getstime(&tval, tstr);
+
+  float tall = 
+    ((float) (rusage.ru_utime.tv_usec + rusage.ru_stime.tv_usec) / 1000000) +
+    ((float) (rusage.ru_utime.tv_sec + rusage.ru_stime.tv_sec));
+  float nps = n/tdiff;
+
+  printf("%s\t__time_insertion__\tn %lld\tnps %.0f\tcpu(s) %.3f\tcpu_d(s) %.3f\n",
+    tstr, n, nps, tall, tdiff);
+}
+
+void* RandomAccessWorker(void* args) {
+  int id = *(int *)args;
+  struct drand48_data buffer;
+  srand48_r(time(NULL) + pthread_self(), &buffer);
+  long int r;
+  lrand48_r(&buffer, &r);
+
+  long long s = 0;
+  for (long long i = 0; i < num_operation; i++) {
+    s += h.GetDat(strings[(r + i) % n]);
+  }
+
+  char tmp[100];
+  sprintf(tmp, "%lld\n", s);
+}
+
+void RandomAccessTest(int num_threads) {
+  struct drand48_data buffer;
+  srand48_r(time(NULL), &buffer);
+
+  // Begin Test
+  timespec start_time;
+  clock_gettime(CLOCK_REALTIME, &start_time);
+  float fs = getcputime();
+
+  if (num_threads == 1) {
+    long long s = 0;
+    THash<TInt, TInt> stat;
+    long int r;
+    for (long long i = 0; i < num_operation; i++) {
+      s += h.GetDat(strings[i%n]);
+    }
+
+    char tmp[100];
+    sprintf(tmp, "%lld\n", s);
+  } else {
+    pthread_t threads[num_threads];
+    for (int i = 0; i < num_threads; i++) {
+      pthread_create(threads + i, NULL, RandomAccessWorker, NULL);
+    }
+    
+    for (int i = 0; i < num_threads; i++) {
+      pthread_join(threads[i], NULL);
+    }
+  }
+
+  // Calculate Stat
+  struct rusage rusage;
+  getrusage(RUSAGE_SELF, &rusage);
+
+  timespec end_time;
+  clock_gettime(CLOCK_REALTIME, &end_time);
+  float fe = getcputime();
+  float tdiff = fe - fs;
+  float wtdiff = gettimediff(start_time, end_time);
+
+  struct timeval tval;
+  char tstr[64];
+  gettimeofday(&tval, 0);
+  getstime(&tval, tstr);
+
+  float tall = 
+    ((float) (rusage.ru_utime.tv_usec + rusage.ru_stime.tv_usec) / 1000000) +
+    ((float) (rusage.ru_utime.tv_sec + rusage.ru_stime.tv_sec));
+  float nps = num_operation / wtdiff;
+
+  printf("%s\t__time_random_access__\tn %lld\tnpst %.0f\tcpu(s) %.3f\twtime_d(s) %.3f\n",
+    tstr, num_operation * num_threads, nps, tall, wtdiff);
+}
+
+
+int main( int argc, char* argv[] ){
+  if (argc != 2) {
+    exit(0);
+  }
+  sscanf(argv[1], "%lld", &n);
+
+  RandomStrings();
+
+  // Insertion Test
+  InsertionTest();
+  
+  // Random Access Test
+  TRnd rnd;
+  strings.Shuffle(rnd);
+  int a[] = {1,2,4,10,20,40,80,160};
+  for (int i = 0; i < 8; i++) {
+    RandomAccessTest(a[i]);
+  }
+
+  return 0;
+}
