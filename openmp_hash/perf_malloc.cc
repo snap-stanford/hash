7659218b213cf17af17dd0ba264999ea2a1b34b4
Saachi Jain <saachi@stanford.edu>
2016-11-02 12:01:42 -0700

    condense perf_malloc

--- openmp_hash/perf_malloc.cc
+++ openmp_hash/perf_malloc.cc
@@ -10,6 +10,8 @@
 #include <sys/time.h>
 #include <sys/times.h>
 
+int repeat_times = 3;
+
 float gettimediff(timespec start, timespec end) {
   timespec temp; 
   if ((end.tv_nsec-start.tv_nsec)<0) {
@@ -22,42 +24,44 @@ float gettimediff(timespec start, timespec end) {
   return (float)temp.tv_sec + ((float)temp.tv_nsec) / 1000000000;
 }
 
-
-char* perform_malloc(long long size) {
+void perform_malloc(long long size) {
+  float total_malloc = 0;
+  float total_free = 0;
   timespec start_time;
-  clock_gettime(CLOCK_REALTIME, &start_time);
-  char* buf = new char[size];
   timespec end_time;
-  clock_gettime(CLOCK_REALTIME, &end_time);
-  float wtdiff = gettimediff(start_time, end_time);
-  std::cout<<"malloc: " << wtdiff <<std::endl;
-  return buf;
-}
-
-void perform_free(char* buf) {
-  timespec start_time;
-  clock_gettime(CLOCK_REALTIME, &start_time);
-  delete[] buf;
-  timespec end_time;
-  clock_gettime(CLOCK_REALTIME, &end_time);
-  float wtdiff = gettimediff(start_time, end_time);
-  std::cout<<"free: " << wtdiff <<std::endl;
+  for (int i=0; i<repeat_times; i++) {
+    clock_gettime(CLOCK_REALTIME, &start_time);
+    char* buf = new char[size];
+    clock_gettime(CLOCK_REALTIME, &end_time);
+    total_malloc += gettimediff(start_time, end_time);
+    buf[0] = 0;
+    clock_gettime(CLOCK_REALTIME, &start_time);
+    delete[] buf;
+    clock_gettime(CLOCK_REALTIME, &end_time);
+    total_malloc += gettimediff(start_time, end_time);
+  }
+  std::cout << "malloc: " << total_malloc/repeat_times << " free: " << total_free/repeat_times << std::endl;
 }
 
 void perform_tvec(long long size) {
+  float total_alloc = 0;
+  float total_destroy = 0;
   timespec start_time;
-  clock_gettime(CLOCK_REALTIME, &start_time);
-  TVec<TInt, int64> v(size);
   timespec end_time;
-  clock_gettime(CLOCK_REALTIME, &end_time);
-  float wtdiff = gettimediff(start_time, end_time);
-  v[0] = 0;
-  timespec start_time;
-  clock_gettime(CLOCK_REALTIME, &start_time);
-  ~v;
-  timespec end_time;
-  clock_gettime(CLOCK_REALTIME, &end_time);
-  float wtdiff = gettimediff(start_time, end_time);
+  size_t elem_size = sizeof(TInt);
+  long long elems = size/elem_size;
+  for (int i=0; i<repeat_times; i++) {
+    clock_gettime(CLOCK_REALTIME, &start_time);
+    TVec<TInt, int64> v(elems);
+    clock_gettime(CLOCK_REALTIME, &end_time);
+    total_alloc += gettimediff(start_time, end_time);
+    v[0] = 0;
+    clock_gettime(CLOCK_REALTIME, &start_time);
+    ~v;
+    clock_gettime(CLOCK_REALTIME, &end_time);
+    total_destroy += gettimediff(start_time, end_time);
+  }
+  std::cout << "alloc: " << total_alloc/repeat_times << " destroy: " << total_destroy/repeat_times << std::endl;
 }
 
 int main( int argc, char* argv[] ){
@@ -67,16 +71,10 @@ int main( int argc, char* argv[] ){
   }
   long long max_size;
   sscanf(argv[1], "%lld", &max_size);
-  size_t elem_size = sizeof(TInt);
   for (long long i=10000; i<= max_size; i=i*10) {
     std::cout<<i<<std::endl;
-    long long elems = i/elem_size;
-    for (int j = 0; j<5; j++) {
-      char* buf = perform_malloc(i);
-      char[0] = 0;
-      perform_free(buf);
-      perform_tvc(elems);
-    }
+    perform_malloc(i);
+    perform_tvec(i);
   }
  return 0;
 }
